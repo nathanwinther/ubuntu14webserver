57b5304ad2ba042735f2bb09770529e1cb1408bd
Nathan Winther <nwinther@taunton.com>
2016-06-10 13:38:55 -0400

    mysql defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 default['ubuntu14webserver']['chef_run_once_directory'] = '/var/chef/run_once'
 
 # database
-default['ubuntu14webserver']['database']['bind_address'] = '0.0.0.0'
-default['ubuntu14webserver']['database']['max_allowed_packet'] = '64M'
+default['ubuntu14webserver']['database']['bind_address'] = '127.0.0.1'
+default['ubuntu14webserver']['database']['max_allowed_packet'] = '16M'
 default['ubuntu14webserver']['database']['root_password'] = ''
-default['ubuntu14webserver']['database']['remote_root'] = true
+default['ubuntu14webserver']['database']['remote_root'] = false
 
 # web
 default['ubuntu14webserver']['web']['apache_run_user'] = 'www-data'
